fd2888a7e6f6c8c4bbc35c8066a386b77db2cf73
George Tang <georgecctang@gmail.com>
2020-11-13 01:07:52 +0000

    create sessions

--- config/routes.rb
+++ config/routes.rb
@@ -19,13 +19,18 @@ Rails.application.routes.draw do
   end
 
   # Route for about page
-  get 'about', to: 'abouts#show'
+  get '/about', to: 'abouts#show'
 
   # These routes will be for signup. The first renders a form in the browse, the second will 
   # receive the form and create a user in our database using the data given to us by the user.
   get '/signup', to: 'users#new'
   post '/users', to: 'users#create'
   
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
