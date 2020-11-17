c8eea30244dd982e50d6e279b07ef01504c5f5af
George Tang <georgecctang@gmail.com>
2020-11-17 21:11:31 +0000

    create feature home_page specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,6 @@ RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
   describe "Validations" do
-<<<<<<< HEAD
-=======
   
     it "is invalid if password is an empty string" do
       @user = User.new(first_name: 'John', last_name: 'Doe', email: 'test@test.com', password: '', password_confirmation: '')
@@ -48,13 +46,11 @@ RSpec.describe User, type: :model do
       expect(@user.save).to be false
       expect(@user.errors.messages[:password][0]).to include("is too short")
     end
->>>>>>> feature/user-specs
 
     it "is valid if all fields are valid" do
       @user = User.new(first_name: 'John', last_name: 'Doe', email: 'test@test.com', password: 'abcdefgh', password_confirmation: 'abcdefgh')
       expect(@user).to be_valid
     end
-<<<<<<< HEAD
   
     it "is invalid if password is missing" do
       @user = User.new(first_name: 'John', last_name: 'Doe', email: 'test@test.com', password: 'abcdefgh', password_confirmation: 'abcdefgh')
@@ -69,8 +65,7 @@ RSpec.describe User, type: :model do
       expect(@user.save).to be false
       puts @user.errors.messages
       # expect(@user.errors.messages[:password_confirmation]).to include("can\'t be blank")
-=======
-
+    end
   end
 
   describe '.authenticate_with_credentials' do
@@ -108,9 +103,7 @@ RSpec.describe User, type: :model do
       @user.save
       @user_login = User.authenticate_with_credentials('Test@tEst.coM', 'abcdefgh')
       expect(@user_login).not_to be_nil
->>>>>>> feature/user-specs
     end
-
 
   end
 
