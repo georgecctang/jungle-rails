a7552d34f26e707ae3ee5345b337a38405c2962e
George Tang <georgecctang@gmail.com>
2020-11-17 22:33:59 +0000

    create feature product_details_spec

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They can access product details" do
+    # ACT
+    visit root_path
+
+    # # DEBUG
+    save_screenshot 'product_details_1.png'
+    first(:link, "Details »").click
+
+    
+    # #VERIFY
+    expect(page).to have_css '.products-show'
+    save_screenshot 'product_details_2.png'
+
+  end
+
+
+end
