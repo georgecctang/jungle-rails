84a5fd261209aaf12e9929827dfd00169ef0886c
George Tang <georgecctang@gmail.com>
2020-11-18 00:03:15 +0000

    feature/testing-add-to-cart

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature, js: true do
-
+RSpec.feature "AddToCarts", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -17,16 +16,18 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
     end
   end
 
-  scenario "They can access product details" do
-    # ACT
-    visit root_path
-    first(:link, "Details »").click
+  scenario "They can add item to cart" do
     
-    # #VERIFY
-    expect(page).to have_css '.products-show'
-    save_screenshot 'product_details_1.png'
+    #ACT
+    visit root_path
+    save_screenshot 'add_to_cart_1.png'
+    click_button('Add', :match => :first)
+
+
+    # DEBUG/VERIFY
+    expect(page).to have_text("My Cart (1)")
+    save_screenshot 'add_to_cart_2.png'
 
   end
-
 
 end
